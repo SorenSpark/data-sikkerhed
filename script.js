06985c9609aa42f4b8506b58281b307945e6d37f
SorenSpark <tobias5704@gmail.com>
2024-04-18 08:40:12 +0200

    Opstart af document og js

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+
+
+
+
+
+/* variabler */
+
+const knap = document.querySelectorAll(".accordion-item h2"); /* hentet alle h2'er */
+
+knap.forEach((accordionToggle) => {
+accordionToggle.addEventListener("click", () => {
+    
+    let accordionItem = accordionToggle.parentNode; /* henter elementer ind fra accordiontoggle via parentNode*/
+
+    let accordionContent = accordionToggle.nextElementSibling; /* henter elementer ind fra accordiontoggle via nextElementSibling */
+
+    /* hvis Accordion er åben, så skal den lukkes */
+    if(accordionContent.style.maxHeight){
+    
+        accordionContent.style.maxHeight = null; /* maxhøjde sættes til null= 0px */
+        
+        accordionItem.classList.remove("active"); /* klassen active fjernes fra vores accordion */
+
+    }else{
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + "px"; /* maxhøjde sættes til scrollhøjde + 0px */
+    accordionItem.classList.add("active"); /* klassen active tilføjes til vores accordion */}
+        
+})  
+
+});
